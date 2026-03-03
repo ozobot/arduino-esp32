8e7544b3792f10650f8a80b6ae9b860faab73df1
ondrap <pilat.ondrej@seznam.cz>
2026-03-03 09:47:43 +0100

    circuit_kit: add Ozobot Circuit kit board support

---
+++ variants/ozobot_circuit_kit/io_pin_remap.cpp
@@ -0,0 +1,82 @@
+#if defined(BOARD_HAS_PIN_REMAP) && !defined(ARDUINO_CORE_BUILD)
+// -DARDUINO_CORE_BUILD must be set for core files only, to avoid extra
+// remapping steps that would create all sorts of issues in the core.
+// Removing -DBOARD_HAS_PIN_REMAP at least does correctly restore the
+// use of GPIO numbers in the API.
+#error This build system is not supported. Please rebuild without BOARD_HAS_PIN_REMAP.
+#endif
+
+#if !defined(BOARD_HAS_PIN_REMAP)
+// This board uses pin mapping but the build system has disabled it
+#warning The build system forces the Arduino API to use GPIO numbers on a board that has custom pin mapping.
+#elif defined(BOARD_USES_HW_GPIO_NUMBERS)
+// The user has chosen to disable pin mappin.
+#warning The Arduino API will use GPIO numbers for this build.
+#endif
+
+#include "Arduino.h"
+
+// NOTE: This must match with the remapped pin sequence in pins_arduino.h
+static const int8_t TO_GPIO_NUMBER[] = {
+    35 , // [ 0] G0
+    36 , // [ 1] G1
+    37 ,  // [ 2] G2
+    38 ,  // [ 3] G3
+    45 ,  // [ 4] G4
+    46 ,  // [ 5] G5
+    39 ,  // [ 6] G6
+    40 , // [ 7] G7
+    41 , // [ 8] G8
+    42 , // [ 9] G9
+    18 , // [10] Motor L IN1
+    17 , // [11] Motor L IN2
+    21 , // [12] Motor R IN1
+    33 , // [13] Motor R IN2
+    34 , // [14] SPI CS
+    12 ,  // [15] SPI CLK
+    11 , // [16] SPI MOSI
+    13 ,  // [17] SPI MISO
+    47 ,  // [18] I2C SDA
+    48 ,  // [19] I2C SCK
+    14 ,  // [20] I2C Interrupt
+    43 , // [21] UART TX
+    44 , // [22] UART RX
+    1, // [23] ADC Channel 0
+    2, // [24] ADC Channel 1
+    3, // [25] ADC Channel 2
+    4, // [26] ADC Channel 3
+    5, // [27] ADC Channel 4
+    6, // [28] ADC Channel 5
+    7, // [29] ADC Channel 6
+    8, // [30] ADC Channel 7
+    9, // [31] ADC Channel 8
+    10, // [32] ADC Channel 9
+};
+
+static const unsigned DIGITAL_PINS_COUNT = sizeof(TO_GPIO_NUMBER) / sizeof(TO_GPIO_NUMBER[0]);
+
+#if defined(BOARD_HAS_PIN_REMAP) && !defined(BOARD_USES_HW_GPIO_NUMBERS)
+
+int8_t digitalPinToGPIONumber(int8_t digitalPin)
+{
+    if ((digitalPin < 0) || (digitalPin >= DIGITAL_PINS_COUNT))
+        return -1;
+    return TO_GPIO_NUMBER[digitalPin];
+}
+
+int8_t gpioNumberToDigitalPin(int8_t gpioNumber)
+{
+    if (gpioNumber < 0)
+        return -1;
+
+    // slow linear table lookup
+    for (int8_t digitalPin = 0; digitalPin < DIGITAL_PINS_COUNT; ++digitalPin) {
+        if (TO_GPIO_NUMBER[digitalPin] == gpioNumber)
+            return digitalPin;
+    }
+
+    // not found
+    return -1;
+}
+
+#endif
---
+++ variants/ozobot_circuit_kit/pins_arduino.h
@@ -0,0 +1,128 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+
+#define USB_VID 0x2D81
+#define USB_PID 0x1901
+
+#ifndef __cplusplus
+#define constexpr const
+#endif
+
+#if defined(BOARD_HAS_PIN_REMAP) && !defined(BOARD_USES_HW_GPIO_NUMBERS)
+
+// Arduino style definitions (API uses Dx)
+
+static constexpr uint8_t D0         = 0; // G0
+static constexpr uint8_t D1         = 1; // G1
+static constexpr uint8_t D2         = 2; // G2
+static constexpr uint8_t D3         = 3; // G3
+static constexpr uint8_t D4         = 4; // G4
+static constexpr uint8_t D5         = 5; // G5
+static constexpr uint8_t D6         = 6; // G6
+static constexpr uint8_t D7         = 7; // G7
+static constexpr uint8_t D8         = 8; // G8
+static constexpr uint8_t D9         = 9; // G9
+static constexpr uint8_t D10        = 10; // Motor L IN1
+static constexpr uint8_t D11        = 11; // Motor L IN2
+static constexpr uint8_t D12        = 12; // Motor R IN1
+static constexpr uint8_t D13        = 13; // Motor R IN2
+static constexpr uint8_t D14        = 14; // SPI CS
+static constexpr uint8_t D15        = 15; // SPI CLK
+static constexpr uint8_t D16        = 16; // SPI MOSI
+static constexpr uint8_t D17        = 17; // SPI MISO
+static constexpr uint8_t D18        = 18; // I2C SDA
+static constexpr uint8_t D19        = 19; // I2C SCK
+static constexpr uint8_t D20        = 20; // I2C Interrupt
+static constexpr uint8_t D21        = 21; // UART TX
+static constexpr uint8_t D22        = 22; // UART RX
+
+static constexpr uint8_t A0         = 23; // also DTR
+static constexpr uint8_t A1         = 24;
+static constexpr uint8_t A2         = 25;
+static constexpr uint8_t A3         = 26;
+static constexpr uint8_t A4         = 27; // also SDA
+static constexpr uint8_t A5         = 28; // also SCL
+static constexpr uint8_t A6         = 29;
+static constexpr uint8_t A7         = 30;
+static constexpr uint8_t A8         = 31;
+static constexpr uint8_t A9         = 32;
+
+#else
+
+// ESP32-style definitions (API uses GPIOx)
+
+static constexpr uint8_t D0         = 35; // G0
+static constexpr uint8_t D1         = 36; // G1
+static constexpr uint8_t D2         = 37; // G2
+static constexpr uint8_t D3         = 38; // G3
+static constexpr uint8_t D4         = 45; // G4
+static constexpr uint8_t D5         = 46; // G5
+static constexpr uint8_t D6         = 39; // G6
+static constexpr uint8_t D7         = 40; // G7
+static constexpr uint8_t D8         = 41; // G8
+static constexpr uint8_t D9         = 42; // G9
+static constexpr uint8_t D10        = 18; // Motor L IN1
+static constexpr uint8_t D11        = 17; // Motor L IN2
+static constexpr uint8_t D12        = 21; // Motor R IN1
+static constexpr uint8_t D13        = 33; // Motor R IN2
+static constexpr uint8_t D14        = 34; // SPI CS
+static constexpr uint8_t D15        = 12; // SPI CLK
+static constexpr uint8_t D16        = 11; // SPI MOSI
+static constexpr uint8_t D17        = 13; // SPI MISO
+static constexpr uint8_t D18        = 47; // I2C SDA
+static constexpr uint8_t D19        = 48; // I2C SCK
+static constexpr uint8_t D20        = 14; // I2C Interrupt
+static constexpr uint8_t D21        = 43; // UART TX
+static constexpr uint8_t D22        = 44; // UART RX
+
+static constexpr uint8_t A0         = 1;
+static constexpr uint8_t A1         = 2;
+static constexpr uint8_t A2         = 3;
+static constexpr uint8_t A3         = 4;
+static constexpr uint8_t A4         = 5;
+static constexpr uint8_t A5         = 6;
+static constexpr uint8_t A6         = 7;
+static constexpr uint8_t A7         = 8;
+static constexpr uint8_t A8         = 9;
+static constexpr uint8_t A9         = 10;
+
+#endif
+
+// alternate pin functions
+
+// static constexpr uint8_t LED_BUILTIN = D13;
+
+static constexpr uint8_t TX   = D21;
+static constexpr uint8_t RX   = D22;
+// static constexpr uint8_t RTS  = LED_BLUE;
+// static constexpr uint8_t CTS  = D3;
+// static constexpr uint8_t DTR  = A0;
+// static constexpr uint8_t DSR  = D4;
+
+static constexpr uint8_t SS   = D14;
+static constexpr uint8_t MOSI = D16;
+static constexpr uint8_t MISO = D17;
+static constexpr uint8_t SCK  = D15;
+
+static constexpr uint8_t SDA  = D18;
+static constexpr uint8_t SCL  = D19;
+
+static constexpr uint8_t MOTOR_L_IN1 = D10;
+static constexpr uint8_t MOTOR_L_IN2 = D11;
+
+static constexpr uint8_t MOTOR_R_IN1 = D12;
+static constexpr uint8_t MOTOR_R_IN2 = D13;
+
+// #define PIN_I2S_SCK     D7
+// #define PIN_I2S_FS      D8
+// #define PIN_I2S_SD      D9
+// #define PIN_I2S_SD_OUT  D9 // same as bidir
+// #define PIN_I2S_SD_IN   D10
+
+#ifndef __cplusplus
+#undef constexpr
+#endif
+
+#endif /* Pins_Arduino_h */
